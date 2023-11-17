e702998c3ad86120c93d2a800d7aae6c65856a51
pheno <jonasalab@gmail.com>
2023-11-17 16:18:59 +0100

    implemented Flyway for managing database schema and data scripts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,1 @@
-INSERT INTO hero_entity (name) VALUES ('Ironman');
-INSERT INTO hero_entity (name) VALUES ('Superman');
-INSERT INTO hero_entity (name) VALUES ('Batman');
+
---
+++ src/main/resources/db/migration/V1__create_database.sql
@@ -0,0 +1,2 @@
+--En caso de migrar a otro sistema en el futuro, ajustar este script ej: mysql
+CREATE SCHEMA IF NOT EXISTS SUPERHEROESDB;
